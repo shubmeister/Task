5e861424c7a35b76f05e85a8ffa86da36a0df5dc
shubmeister <negishub888@gmail.com>
2025-07-26 08:12:56 +0530

    Created a patient from to accept the details

--- patient-registration/src/db.js
+++ patient-registration/src/db.js
@@ -2,8 +2,17 @@ import {PGlite} from 'pg-lite';
 
 const db = new PGlite;
 
-await db.exec('CREATE TABLE IF NOT EXISTS
+await db.exec( `CREATE TABLE IF NOT EXISTS
     patients(
 
-    )
-    ');
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        patientid INTEGER;
+        name TEXT NOT null;
+        age INTEGER;
+        gender TEXT;
+        address TEXT;
+
+            )`
+            );
+
+    export default db;
